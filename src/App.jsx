19cbb33c16979221ca4e05456f117b5a087d6696
mateuszPATeam <60876661+mateuszPATeam@users.noreply.github.com>
2023-04-27 08:46:37 +0200

    add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
-import { FormControl, FormLabel, Input, Select } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+  Button,
+} from "@chakra-ui/react";
 
 function App() {
+  function submitForm(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    console.log([...formData]);
+  }
+
   return (
     <main>
-      <form>
+      <form onSubmit={submitForm}>
         <FormControl>
           <FormLabel htmlFor="inputTest">Test Input</FormLabel>
           <Input name="inputTest" id="inputTest" type="text" />
         </FormControl>
         <FormControl>
           <FormLabel htmlFor="selectTest">Countries</FormLabel>
-          <Select id="selectTest">
+          <Select id="selectTest" name="selectTest">
             <option value=""></option>
             <option value="PL">Poland</option>
             <option value="US">United States</option>
           </Select>
         </FormControl>
+        <Button colorScheme="green" type="submit">
+          Submit
+        </Button>
       </form>
     </main>
   );
